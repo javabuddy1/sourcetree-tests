45647f30dd541089f4ae244e24aeda7699ba88f7
javabuddy1 <javabuddy1@gmail.com>
2024-02-24 15:13:40 -0500

    even works with visual source code git lens

--- HelloWorld/src/main/java/org/example/Main.java
+++ HelloWorld/src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is line 2");
-        System.out.println("This is line 3cccc");
+        System.out.println("This is line 3");
     }
 }
