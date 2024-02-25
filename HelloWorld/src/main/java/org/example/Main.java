affd44a6a97bb739fe05c4fde2e546fbb78481e5
javabuddy1 <javabuddy1@gmail.com>
2024-02-25 08:31:41 -0500

    Create Runner class
    Create run method so that we are working in a non-static method

--- HelloWorld/src/main/java/org/example/Main.java
+++ HelloWorld/src/main/java/org/example/Main.java
@@ -2,8 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("This is line 2");
-        System.out.println("This is line 3");
+        Runner runner = new Runner();
+        runner.run();
     }
 }
---
+++ HelloWorld/src/main/java/org/example/Runner.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Runner {
+    public void run() {
+        System.out.println("Hello world!");
+        System.out.println("This is line 2");
+        System.out.println("This is line 3");
+    }
+}
